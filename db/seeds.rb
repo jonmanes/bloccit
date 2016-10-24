6f57faf66895d7f23f4bbbfab29f760ab4610255
jon manes <jon.manes@gmail.com>
2016-10-23 21:33:38 -0500

    Assignment 17

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,10 @@ posts = Post.all
   )
 end
 
+Post.find_or_create_by(title: "This is a test of the emergency broadcast...", body: "This is a test of the emergency broadcast system...")
+uniq_post = Post.find_by_title("This is a test of the emergency broadcast...")
+Comment.find_or_create_by(post: uniq_post, body: "Had this been an actual emergency the noise you just heard...")
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
